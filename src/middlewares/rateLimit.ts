b4d386c0ba05da12a167a67cb3b4295eb362f6b7
zorotvapi <zorotvllc@gmail.com>
2025-02-09 12:29:25 +0500

    Increase Rate Limit

--- src/middlewares/rateLimit.ts
+++ src/middlewares/rateLimit.ts
@@ -1,10 +1,9 @@
 import rateLimit from "express-rate-limit";
 
 export const limiter = rateLimit({
-  windowMs: 60 * 60 * 1000, // 60 minutes
-  max: 50, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-  standardHeaders: "draft-7", // Set `RateLimit` and `RateLimit-Policy`` headers
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10000, // Limit each IP to 10,000 requests per 15 minutes
+  standardHeaders: "draft-7", // Set `RateLimit` and `RateLimit-Policy` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-  message:
-    "Too many accounts created from this IP, please try again after 1 hour",
+  message: "Too many requests from this IP, please try again after 15 minutes",
 });
